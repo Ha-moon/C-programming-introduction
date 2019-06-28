76a37b7db74b8331902e60f9470b3bee348a616e
Ha-moon <43697574+Ha-moon@users.noreply.github.com>
2019-06-28 15:10:15 +0900

    spectification(명세)과 implementation(구현)의 분리

---
+++ hw10/a.cpp
@@ -0,0 +1,10 @@
+#include "shape.h"
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	CShape *sptr = new CCircle(2, 3, 1); //base class의 pointer가
+	CShape *sptr2 = new CRect(2, 3, 4, 5); //아들 class의 object를 가리킴
+	sptr->Move(10, 10); sptr->Print();
+	sptr2->Move(100, 100); sptr2->Print();
+}
