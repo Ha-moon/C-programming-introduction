b4df149fe28d225bb39ae112f2d3dc8eb9dbbeaa
Ha-moon <43697574+Ha-moon@users.noreply.github.com>
2019-06-28 15:10:35 +0900

    Create shape.cpp

---
+++ hw10/shape.cpp
@@ -0,0 +1,13 @@
+#include "shape.h"
+#include <iostream>
+using namespace std;
+void CShape::Move(float a, float b) { x += a; y += b; }
+void CShape::Print() {
+	cout << "중심=(" << x << ", " << y << ")" << endl;
+}
+void CCircle::Print() {
+	cout<<"원중심=(" << x << ", " << y << ") 반지름=" << Radius<< endl;
+}
+void CRect::Print() {
+	cout<<"직사각형중심=(" << x << ", " << y << ") 가로=" << Garo<<" 세로=" << Sero << endl;
+}
